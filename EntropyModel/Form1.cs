ebebbd4003423221ef96c5c66c1c9c000b8a854b
Dmitriy Pavlov <dmitriy.pavlov@danfoss.com>
2021-07-30 13:47:41 +0300

    Intro

--- EntropyModel/Form1.cs
+++ EntropyModel/Form1.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+/* Урок "Механика столкновений. Игры и хаос."
+ * Все уроки на http://digitalmodels.ru
+ * 
+ */
 
 namespace EntropyModel
 {
--- EntropyModel/Properties/AssemblyInfo.cs
+++ EntropyModel/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EntropyModel")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DigitalModels")]
 [assembly: AssemblyProduct("EntropyModel")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright © DigitalModels 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
